2bdb5c0f5ef67ba3a7738efccedc3bc57690ab81
fgeai <fgeaiessah@gmail.com>
2018-08-05 02:02:33 +0200

    Init Commit

--- CrossSolar/Domain/CrossSolarDbContext.cs
+++ CrossSolar/Domain/CrossSolarDbContext.cs
@@ -22,6 +22,42 @@ namespace CrossSolar.Domain
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<OneDayElectricityModel>(e => e.Property(s => s.Sum).HasComputedColumnSql("SELECT SUM(dbo.OneHourElectricitys.KiloWatt) From dbo.OneHourElectricitys" +
+
+    " Where(DATEPART(DAY, dbo.OneHourElectricitys.DateTime) = DATEPART(DAY, "+ e.Property(s => s.DateTime)+ ")" +
+
+    " and(DATEPART(MONTH, dbo.OneHourElectricitys.DateTime) = DATEPART(MONTH, " + e.Property(s => s.DateTime) + ")" +
+
+    " and(DATEPART(YEAR, dbo.OneHourElectricitys.DateTime) = DATEPART(YEAR, " + e.Property(s => s.DateTime) + ")"));
+
+            modelBuilder.Entity<OneDayElectricityModel>(e => e.Property(s => s.Average).HasComputedColumnSql("SELECT AVG(dbo.OneHourElectricitys.KiloWatt) From dbo.OneHourElectricitys" +
+
+     " Where(DATEPART(DAY, dbo.OneHourElectricitys.DateTime) = DATEPART(DAY, " + e.Property(s => s.DateTime) + ")" +
+
+     " and(DATEPART(MONTH, dbo.OneHourElectricitys.DateTime) = DATEPART(MONTH, " + e.Property(s => s.DateTime) + ")" +
+
+     " and(DATEPART(YEAR, dbo.OneHourElectricitys.DateTime) = DATEPART(YEAR, " + e.Property(s => s.DateTime) + ")"));
+
+            modelBuilder.Entity<OneDayElectricityModel>(e => e.Property(s => s.Maximum).HasComputedColumnSql("SELECT MAX(dbo.OneHourElectricitys.KiloWatt) From dbo.OneHourElectricitys" +
+
+    " Where(DATEPART(DAY, dbo.OneHourElectricitys.DateTime) = DATEPART(DAY, " + e.Property(s => s.DateTime) + ")" +
+
+    " and(DATEPART(MONTH, dbo.OneHourElectricitys.DateTime) = DATEPART(MONTH, " + e.Property(s => s.DateTime) + ")" +
+
+    " and(DATEPART(YEAR, dbo.OneHourElectricitys.DateTime) = DATEPART(YEAR, " + e.Property(s => s.DateTime) + ")"));
+
+            modelBuilder.Entity<OneDayElectricityModel>(e => e.Property(s => s.Minimum).HasComputedColumnSql("SELECT MIN(dbo.OneHourElectricitys.KiloWatt) From dbo.OneHourElectricitys" +
+
+    " Where(DATEPART(DAY, dbo.OneHourElectricitys.DateTime) = DATEPART(DAY, " + e.Property(s => s.DateTime) + ")" +
+
+    " and(DATEPART(MONTH, dbo.OneHourElectricitys.DateTime) = DATEPART(MONTH, " + e.Property(s => s.DateTime) + ")" +
+
+    " and(DATEPART(YEAR, dbo.OneHourElectricitys.DateTime) = DATEPART(YEAR, " + e.Property(s => s.DateTime) + ")"));
+
+
+
         }
+
+
     }
 }
--- CrossSolar/Models/OneDayElectricityModel.cs
+++ CrossSolar/Models/OneDayElectricityModel.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CrossSolar.Domain
 {
     public class OneDayElectricityModel
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public double Sum { get; set; }
-
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public double Average { get; set; }
-
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public double Maximum { get; set; }
-
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public double Minimum { get; set; }
 
         public DateTime DateTime { get; set; }
